47649bee201ffe914ab14080fad0ecf3c2de1ae3
tverdov.konstantin tverdov.konstantin <konstantin08081997@mail.ru>
2022-01-31 20:11:46 +0200

    31.01.2022

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using SqlPerson.Data.Entities;
 using System;
 using System.Collections.Generic;
@@ -11,16 +12,23 @@ namespace SqlPerson.Data
     class AppDBContext : DbContext
     {
         public DbSet<Person> People { get; set; }
-
-        public AppDBContext()
+        //public SqlConnectionStringBuilder stringBuilder;
+        public AppDBContext(/*SqlConnectionStringBuilder sb*/)
         {
             Database.EnsureCreated();
+            //stringBuilder = new SqlConnectionStringBuilder();
+            //stringBuilder.DataSource = sb.DataSource;
+            //stringBuilder.AttachDBFilename = sb.AttachDBFilename;
+            //stringBuilder.IntegratedSecurity = sb.IntegratedSecurity;
+            //stringBuilder.UserID = sb.UserID;
+            //stringBuilder.Password = sb.Password;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.LogTo(Program.Log.Info);
+
             optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\tverd\source\repos\SqlPerson\DBFiles\DBPerson.mdf;Integrated Security=True");
         }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using SqlPerson.Data;
+using Microsoft.Data.SqlClient;
 using System;
 
 namespace SqlPerson
@@ -8,13 +9,24 @@ namespace SqlPerson
         public static NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            
+            //SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            //stringBuilder.AttachDBFilename = @"C:\Users\tverd\source\repos\SqlPerson\DBFiles\DBPerson.mdf";
+            //stringBuilder.DataSource = @"(LocalDB)\MSSQLLocalDB";
+            //stringBuilder.IntegratedSecurity = true;
+            //stringBuilder.UserID = "Gucci";
+            //stringBuilder.Password = "123321";
+            //Console.WriteLine(stringBuilder.ConnectionString);
+            
             Log.Info("Start");
-            using (AppDBContext db = new AppDBContext())
+            using (AppDBContext db = new AppDBContext(/*stringBuilder*/))
             {
-                db.People.Add(new Data.Entities.Person { Name = "Robert", Surname = "Svon", Age = 34, PhoneNumber = "+380508965845" ,Email = "Robert@gmail.com", });
-                db.People.Add(new Data.Entities.Person { Name = "Harry", Surname = "Potter", Age = 40, PhoneNumber = "+380966596585" ,Email = "Harry@gmail.com", });
+                db.People.Add(new Data.Entities.Person { Name = "Robert", Surname = "Svon", Age = 34, PhoneNumber = "+380508965845", Email = "Robert@gmail.com", });
+                db.People.Add(new Data.Entities.Person { Name = "Harry", Surname = "Potter", Age = 40, PhoneNumber = "+380966596585", Email = "Harry@gmail.com", });
                 db.SaveChanges();
             }
+
+
             Console.ReadKey();
         }
     }
